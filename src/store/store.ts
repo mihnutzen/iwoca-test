e807b0c741a51cffccfdecc8ee0d86325947c92f
Mihnea Belcin <mihnea.belcin@gmail.com>
2020-03-03 11:11:59 +0000

    cleanup - removing devtool refrences from store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,19 +1,7 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './root';
 import thunk from 'redux-thunk';
 
-declare global {
-  interface Window {
-    __REDUX_DEVTOOLS_EXTENSION__?: () => any;
-  }
-}
-
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
